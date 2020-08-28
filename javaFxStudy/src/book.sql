97637a571969cd07ff3466c31ad7bf1e578c7140
leadon <leadon@DESKTOP-IEK7BIC>
2020-08-28 18:24:48 +0900

    boof

---
+++ javaFxStudy/src/book.sql
@@ -0,0 +1,13 @@
+select * from tab;
+
+
+--도서정보
+create table book (
+ book_title varchar2(100),
+ author varchar2(100),
+ press varchar2(100),
+ press_date varchar2(10),
+ creation_date date
+);
+
+
