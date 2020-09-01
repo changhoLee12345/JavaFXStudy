cac9140fbd7f3d70a1be955ec275b717770db188
leadon <leadon@DESKTOP-IEK7BIC>
2020-09-01 20:55:55 +0900

    fd

--- javaFxStudy/src/hr/control/ChartController.java
+++ javaFxStudy/src/hr/control/ChartController.java
@@ -11,8 +11,8 @@ import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
-import javafx.scene.chart.PieChart.Data;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 
 public class ChartController implements Initializable {
 
@@ -26,37 +26,67 @@ public class ChartController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		pieChart.setData(FXCollections.observableArrayList(new PieChart.Data("AWT", 10), new PieChart.Data("Swing", 30),
-				new PieChart.Data("SWT", 25), new PieChart.Data("JavaFX", 35)));
+		pieChart.setData(FXCollections.observableArrayList( //
+				new PieChart.Data("AWT", 10), new PieChart.Data("Swing", 30), //
+				new PieChart.Data("SWT", 25), new PieChart.Data("JavaFX", 35))//
+		);
 
-//		Series<Axis<String>, Axis<Integer>> series1 = new XYChart.Series<>();
 		XYChart.Series<String, Integer> series1 = new XYChart.Series<>();
 		series1.setName("Men");
-		series1.setData(FXCollections.observableArrayList(//
-				new XYChart.Data<String, Integer>("2015", 70), //
-				new XYChart.Data<String, Integer>("2016", 40), //
-				new XYChart.Data<String, Integer>("2017", 50), //
-				new XYChart.Data<String, Integer>("2018", 30)));
+		series1.setData(FXCollections.observableArrayList(getSeries1()));
 
 		XYChart.Series<String, Integer> series2 = new XYChart.Series<>();
 		series2.setName("Women");
-		series2.setData(FXCollections.observableArrayList(//
-				new XYChart.Data("2015", 30), //
-				new XYChart.Data("2016", 60), //
-				new XYChart.Data("2017", 50), //
-				new XYChart.Data("2018", 60)));
+		series2.setData(FXCollections.observableArrayList(getSeries2()));
 
 		barChart.getData().add(series1);
 		barChart.getData().add(series2);
 
 		XYChart.Series<String, Integer> series3 = new XYChart.Series<>();
 		series3.setName("Celsius");
-		series3.setData(FXCollections.observableArrayList(//
-				new XYChart.Data("2015", 13), //
-				new XYChart.Data("2016", 6), //
-				new XYChart.Data("2017", 22), //
-				new XYChart.Data("2018", 19)));
-
+		series3.setData(FXCollections.observableArrayList(getSeries3()));
 		areaChart.getData().add(series3);
+
+		XYChart.Series<String, Integer> series4 = new XYChart.Series<>();
+		series4.setName("Covid19");
+		series4.setData(FXCollections.observableArrayList(getSeries4()));
+		areaChart.getData().add(series4);
+
+	}
+
+	public ObservableList<XYChart.Data<String, Integer>> getSeries1() {
+		ObservableList<XYChart.Data<String, Integer>> series = FXCollections.observableArrayList();
+		series.add(new XYChart.Data<String, Integer>("2015", 70));
+		series.add(new XYChart.Data<String, Integer>("2016", 40));
+		series.add(new XYChart.Data<String, Integer>("2017", 50));
+		series.add(new XYChart.Data<String, Integer>("2018", 30));
+		return series;
+	}
+
+	public ObservableList<XYChart.Data<String, Integer>> getSeries2() {
+		ObservableList<XYChart.Data<String, Integer>> series = FXCollections.observableArrayList();
+		series.add(new XYChart.Data<String, Integer>("2015", 30));
+		series.add(new XYChart.Data<String, Integer>("2016", 60));
+		series.add(new XYChart.Data<String, Integer>("2017", 50));
+		series.add(new XYChart.Data<String, Integer>("2018", 80));
+		return series;
+	}
+
+	public ObservableList<XYChart.Data<String, Integer>> getSeries3() {
+		ObservableList<XYChart.Data<String, Integer>> series = FXCollections.observableArrayList();
+		series.add(new XYChart.Data<String, Integer>("2015", 13));
+		series.add(new XYChart.Data<String, Integer>("2016", 6));
+		series.add(new XYChart.Data<String, Integer>("2017", 22));
+		series.add(new XYChart.Data<String, Integer>("2018", 19));
+		return series;
+	}
+
+	public ObservableList<XYChart.Data<String, Integer>> getSeries4() {
+		ObservableList<XYChart.Data<String, Integer>> series = FXCollections.observableArrayList();
+		series.add(new XYChart.Data<String, Integer>("2015", 25));
+		series.add(new XYChart.Data<String, Integer>("2016", 45));
+		series.add(new XYChart.Data<String, Integer>("2017", 29));
+		series.add(new XYChart.Data<String, Integer>("2018", 43));
+		return series;
 	}
 }
--- javaFxStudy/src/hr/control/ChartMain.java
+++ javaFxStudy/src/hr/control/ChartMain.java
@@ -11,7 +11,7 @@ public class ChartMain extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		String controlName = "SliderControl";
+		String controlName = "fxml/ChartControl";
 
 //		Parent root = FXMLLoader.load(getClass().getResource(controlName + ".fxml"));
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(controlName + ".fxml"));
